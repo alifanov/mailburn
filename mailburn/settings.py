629739149870b9d6ead4fb88f7ce811fa6f8e88b
vampire <lifanov.a.v@gmail.com>
2014-09-15 13:45:17 +0400

    add style

--- mailburn/settings.py
+++ mailburn/settings.py
@@ -82,3 +82,11 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.6/howto/static-files/
 
 STATIC_URL = '/static/'
+STATIC_ROOT = BASE_DIR + '/static/'
+
+TEMPLATE_DIRS = (
+    os.path.join(BASE_DIR,  'templates'),
+)
+
+MEDIA_URL = '/media/'
+MEDIA_ROOT = '/var/www/mailburn/media/'
