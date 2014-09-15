8ec1f96e21547e25bba27126e5f172ae192c2b45
vampire <lifanov.a.v@gmail.com>
2014-09-15 13:54:33 +0400

    add style

--- app/models.py
+++ app/models.py
@@ -1,3 +1,21 @@
 from django.db import models
 
 # Create your models here.
+
+STATUS_CHOICES = (
+    ('S', u'Send'),
+    ('R', u'Readed')
+)
+
+class Mail(models.Model):
+    subject = models.CharField(max_length=256, verbose_name=u'Subject')
+    text = models.TextField(verbose_name=u'Body')
+    to = models.TextField(verbose_name=u'To')
+    status = models.CharField(max_length=1, choices=STATUS_CHOICES, verbose_name=u'Status')
+
+    def __unicode__(self):
+        return u'{}: {}'.format(self.to, self.subject)
+
+    class Meta:
+        verbose_name = u'Mail message'
+        verbose_name_plural = u'Mail messages'
--- app/views.py
+++ app/views.py
@@ -1,11 +1,14 @@
-from django.views.generic import TemplateView
+from django.views.generic import TemplateView, ListView
 from django.utils.decorators import method_decorator
 from django.contrib.auth.decorators import login_required, permission_required
-
+from app.models import *
 # Create your views here.
 
-class HomeView(TemplateView):
+class HomeView(ListView):
     template_name = 'home.html'
+    model = Mail
+    queryset = Mail.objects.all()
+    context_object_name = 'mails'
 
     @method_decorator(login_required)
     def dispatch(self, *args, **kwargs):
