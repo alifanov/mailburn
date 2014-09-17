c53a0c225ebf55eac36c02069b3b95fcf74d1ce6
vampire <lifanov.a.v@gmail.com>
2014-09-17 14:55:32 +0400

    test django google api

--- app/models.py
+++ app/models.py
@@ -15,6 +15,7 @@ class CredentialsModel(models.Model):
     credential = CredentialsField()
 from south.modelsinspector import add_introspection_rules
 add_introspection_rules([], ["^oauth2client\.django_orm\.CredentialsField"])
+add_introspection_rules([], ["^oauth2client\.django_orm\.FlowField"])
 STATUS_CHOICES = (
     ('S', u'Send'),
     ('R', u'Readed')
