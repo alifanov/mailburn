1ebfbf620200ebc49ceea113f483633f2c57b20a
vampire <lifanov.a.v@gmail.com>
2014-09-19 10:54:13 +0400

    test django google api

--- app/views.py
+++ app/views.py
@@ -139,7 +139,7 @@ class MessageSend(View):
         p = {'access_token': request.GET.get('access_token')}
         if request.GET.get('threadId'):
             p['threadId'] = request.GET.get('threadId')
-        r = requests.post('https://www.googleapis.com/gmail/v1/users/me/messages/send',
+        r = requests.post('https://www.googleapis.com/upload/gmail/v1/users/me/messages/send',
                         params=p, data=request.raw_post_data)
         if r.status_code == 200: return HttpResponse(json.dumps(r.json()['messages']), content_type='application/json')
         return HttpResponse('')
