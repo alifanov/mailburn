f09a4448c53315e640bab2601b1527ef3351c8d4
Lifanov Alexander <lifanov.a.v@gmail.com>
2014-09-22 00:33:37 +0400

    add message array in raw format by thread view

--- app/views.py
+++ app/views.py
@@ -137,8 +137,8 @@ class MessageSend(View):
             p['threadId'] = request.GET.get('threadId')
         if request.GET.get('key'):
             p['key'] = request.GET.get('key')
-        msg = base64.b64decode(json.loads(request.body))
-        raise KeyError(msg)
+#        msg = base64.b64decode(json.loads(request.body))
+        raise KeyError(request.body)
         r = requests.post('https://www.googleapis.com/gmail/v1/users/me/messages/send',
                         params=p, data=request.body, headers={
                 'Authorization': request.META['HTTP_AUTHORIZATION'],
