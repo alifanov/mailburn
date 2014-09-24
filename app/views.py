6bbf64ac923464065f5b978ef8bac31ba350bb3d
alifanov <lifanov.a.v@gmail.com>
2014-09-24 18:02:28 +0400

    add snippet in threads list

--- app/views.py
+++ app/views.py
@@ -146,9 +146,9 @@ class ThreadsGet(View):
                                     msg['data'] = msg['data'].split('View this email\r\nin your browser')[0]
                                 if u'\r\nОтправлено из мобильной Почты Mail.Ru\r\n'.encode('utf-8') in msg['data']:
                                     msg['data'] = msg['data'].split(u'\r\nОтправлено из мобильной Почты Mail.Ru\r\n'.encode('utf-8'))[0]
-                                continue
-                            # if part['mimeType'] == 'text/html':
-                            #     msg['data'] = base64.urlsafe_b64decode(str(part['body']['data']))
+                                break
+                            if part['mimeType'] == 'text/html':
+                                msg['data'] = base64.urlsafe_b64decode(str(part['body']['data']))
                     msgs.append(msg)
             ans['messages'] = msgs
             return HttpResponse(json.dumps(ans), content_type='application/json')
