a6083dfa4157a644ce84175dcd97e2bd33dd858b
alifanov <lifanov.a.v@gmail.com>
2014-09-25 19:26:39 +0400

    ё

--- app/views.py
+++ app/views.py
@@ -171,7 +171,7 @@ class ThreadsGet(View):
                 msg['data'] = re.split(r'On \d{2} [\w]+ \d{4} \d{2}:\d{2}', msg['data'])[0]
                 msg['data'] = re.split(r'\r\nOn [\w]+, [\w]+ \d{2}, \d{4} at \d{2}:\d{2}', msg['data'])[0]
                 msg['data'] = re.split(r'\r\n\d{2}.\d{2}.\d{4}, \d{2}:\d{2},', msg['data'])[0]
-                msg['data'] = re.split(r'\d{2}.\d{2}.\d{4}, \d{2}:\d{2}, \\\"', msg['data'])[0]
+                msg['data'] = re.split(r'\d{2}.\d{2}.\d{4}, \d{2}:\d{2}, \\', msg['data'])[0]
                 msg['data'] = re.split(r'\r\n\s+From: \w+', msg['data'])[0]
                 if u'—\r\nSent from Mailbox' in msg['data']:
                     msg['data'] = msg['data'].split(u'—\r\nSent from Mailbox')[0]
