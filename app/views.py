2aff7e207aa341105495baa76bf77656f0b670d4
alifanov <lifanov.a.v@gmail.com>
2014-09-25 18:38:23 +0400

    add snippet in threads list

--- app/views.py
+++ app/views.py
@@ -164,7 +164,7 @@ class ThreadsGet(View):
                 msg['data'] = re.split('\d{2} (?u)[\w]+ \d{4} (?u)\w{1}., \d{2}:\d{2}', msg['data'], re.U)[0]
                 msg['data'] = re.split(r'\r\n\d{4}-\d{2}-\d{2}', msg['data'])[0]
                 msg['data'] = re.split(r'On \d{2}.\d{2}.\d{2}, \d{2}:\d{2}', msg['data'])[0]
-                msg['data'] = re.split(r'\r\n\d{2}.\d{2}.\d{4}, \d{2}:\d{2}', msg['data'])[0]
+                msg['data'] = re.split(r'\r\n\d{2}.\d{2}.\d{4}, \d{2}:\d{2},', msg['data'])[0]
                 msg['data'] = re.split(r'\r\n\s+From: \w+', msg['data'])[0]
                 if u'\r\n—\r\nSent from Mailbox' in msg['data']:
                     msg['data'] = msg['data'].split(u'\r\n—\r\nSent from Mailbox')[0]
