092a052a1d717ab3db2c9e6c4ae2e29497a2a098
alifanov <lifanov.a.v@gmail.com>
2014-09-28 14:02:06 +0400

    ё

--- app/views.py
+++ app/views.py
@@ -170,6 +170,7 @@ class ThreadsGet(View):
                 msg['data'] = re.split(r'\r\n\d{2}.\d{2}.\d{2}, (?u)[\w]+', msg['data'])[0]
                 msg['data'] = re.split(r'On \d{2} [\w]+ \d{4} \d{2}:\d{2}', msg['data'])[0]
                 msg['data'] = re.split(r'\r\nOn [\w]+, [\w]+ \d{1,2}, \d{4},', msg['data'])[0]
+                msg['data'] = re.split(r'\r\nOn [\w]+ \d{1,2}, \d{4}, at \d{1,2}:\d{1,2}', msg['data'])[0]
                 msg['data'] = re.split(r'\r\nOn [\w]+, [\w]+ \d{1,2}, \d{4} at \d{2}:\d{2}', msg['data'])[0]
                 msg['data'] = re.split(r'\r\n\d{2}.\d{2}.\d{4}, \d{2}:\d{2},', msg['data'])[0]
                 msg['data'] = re.split(r'\r\n\d{2}.\d{2}.\d{4} \d{2}:\d{2} ', msg['data'])[0]
