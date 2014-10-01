78614ebe22faad04cd9f143026debc1dd1c3d70c
alifanov <lifanov.a.v@gmail.com>
2014-10-01 13:31:58 +0400

    ё

--- app/views.py
+++ app/views.py
@@ -138,14 +138,11 @@ class ThreadsGet(View):
                                 for part in msg.parts:
                                     if part.content_type == 'text/plain':
                                         part.body = self.msg_filter(part.body)
-                                        raise KeyError(part.body)
                                         break
                                     if part.content_type == 'text/html':
                                         part.body = self.msg_filter(part.body)
-                                        raise KeyError(part.body)
                             else:
                                 msg.body = self.msg_filter(msg.body)
-                                raise KeyError(msg.body)
                             ans_msg['raw'] = mime.python_message_to_string(msg.to_python_message())
                         else:
                             ans_msg['raw'] = msg_raw
