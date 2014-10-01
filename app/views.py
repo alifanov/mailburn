935bdef76ca9865d6a3f6a226ff581267b5634a6
alifanov <lifanov.a.v@gmail.com>
2014-10-01 13:26:41 +0400

    ё

--- app/views.py
+++ app/views.py
@@ -134,7 +134,7 @@ class ThreadsGet(View):
                         msg_raw = str(mr.json()['raw'])
                         if request.GET.get('decode'):
                             msg = mime.from_string(base64.urlsafe_b64decode(msg_raw))
-                            raise KeyError(msg)
+                            raise KeyError(msg.content_type.is_multipart())
                             if msg.content_type.is_multipart():
                                 for part in msg.parts:
                                     if part == '(text/plain)':
